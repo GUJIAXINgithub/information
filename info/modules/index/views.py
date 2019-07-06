70c4c126304e2d7c9a22ad96ac192b9d165a58c0
GUJIAXIN <se7en.ac@foxmail.com>
2019-07-06 19:11:21 +0800

    网站图标展示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, current_app
 
 from info import redis_store
 from info.modules.index import index_blue
@@ -7,3 +7,9 @@ from info.modules.index import index_blue
 @index_blue.route('/')
 def index():
     return render_template('news/index.html')
+
+
+@index_blue.route('/favicon.ico')
+def favicon():
+    # send_static_file是flask中请求静态文件的方法
+    return current_app.send_static_file('news/favicon.ico')
