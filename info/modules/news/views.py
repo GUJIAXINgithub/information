4ca36870f63ebb853513a0543be9ae202cbe3eea
GUJIAXIN <se7en.ac@foxmail.com>
2019-07-10 12:42:07 +0800

    实现新闻详情页登录效果和点击排行逻辑

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,6 +1,9 @@
-from flask import render_template
+from flask import render_template, session, current_app, jsonify
 
+from info import constants
+from info.models import User, News
 from info.modules.news import news_blue
+from info.utils.response_code import RET
 
 
 @news_blue.route('/<int:news_id>')
@@ -10,8 +13,32 @@ def news_detail(news_id):
     :param news_id:
     :return:
     """
-    data = {
+    # 从session中获取用户的id
+    user = None
+    user_id = session.get('id', None)
 
+    # 通过id获取用户信息，传给后台
+    if user_id:
+        try:
+            user = User.query.filter(User.id == user_id).first()
+        except Exception as e:
+            current_app.logger.error(e)
+            return jsonify(errno=RET.DBERR, errmsg='查询失败')
+
+    # 获取点击排行
+    news_li = list()
+    try:
+        news_li = News.query.order_by(News.clicks.desc()).limit(constants.CLICK_RANK_MAX_NEWS)
+    except Exception as e:
+        current_app.logger.error(e)
+
+    news_dict_li = list()
+    for news in news_li:
+        news_dict_li.append(news.to_basic_dict())
+
+    data = {
+        "user": user.to_dict() if user else None,
+        'news_dict_li': news_dict_li
     }
 
     return render_template('news/detail.html', data=data)
