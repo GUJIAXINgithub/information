ca19dc98b6d4b29738108bd316d0739aabaf5061
GUJIAXIN <se7en.ac@foxmail.com>
2019-07-11 21:28:21 +0800

    点击跳转用户中心逻辑实现&用户中心显示登录状态逻辑实现

--- info/modules/center/views.py
+++ info/modules/center/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, g, redirect
 
 from info.modules.center import center_blue
 from info.utils.common import user_login_data
@@ -7,7 +7,15 @@ from info.utils.common import user_login_data
 @center_blue.route('/info')
 @user_login_data
 def user_info():
-    data = {
+    """
+    用户中心
+    :return:
+    """
+    user = g.user
+    if not user:
+        return redirect('/')
 
+    data = {
+        'user': user.to_dict()
     }
     return render_template('news/user.html', data=data)
