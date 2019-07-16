ebddad46115fcbc98fb3d8e915e4a8fe5e9bc491
GUJIAXIN <se7en.ac@foxmail.com>
2019-07-16 12:54:12 +0800

    配置生产环境&修复第三方文本编辑器GUB

--- manage.py
+++ manage.py
@@ -1,11 +1,10 @@
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
-from info import create_app, db
-from info.models import User
+from info import create_app, db, models
 
 # 通过指定配置的名字，创建对应配置的app
 
-app = create_app('development')
+app = create_app('production')
 
 # 创建终端命令对象
 manager = Manager(app)
@@ -28,7 +27,7 @@ def createsuperuser(user_name, password):
         print('缺少参数：-n "用户名" -p "密码"')
 
     # 初始化用户对象
-    user = User()
+    user = models.User()
     user.mobile = user_name
     user.nick_name = user_name
     user.password = password
@@ -45,4 +44,3 @@ def createsuperuser(user_name, password):
 
 if __name__ == '__main__':
     manager.run()
-    # app.run()
